97ae438c5c09ff4201dc3955c2c46888395abcf5
Atnode <git@atnode.fr>
2019-12-02 19:33:44 +0100

    Adding gozimhttpd files

---
+++ main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"strconv"
+
+	"github.com/GeertJohan/go.rice"
+	"github.com/akhenakh/gozim"
+	"github.com/blevesearch/bleve"
+	"github.com/hashicorp/golang-lru"
+
+	_ "github.com/blevesearch/bleve/analysis/lang/ar"
+	_ "github.com/blevesearch/bleve/analysis/lang/cjk"
+	_ "github.com/blevesearch/bleve/analysis/lang/ckb"
+	_ "github.com/blevesearch/bleve/analysis/lang/en"
+	_ "github.com/blevesearch/bleve/analysis/lang/fa"
+	_ "github.com/blevesearch/bleve/analysis/lang/fr"
+	_ "github.com/blevesearch/bleve/analysis/lang/hi"
+	_ "github.com/blevesearch/bleve/analysis/lang/it"
+	_ "github.com/blevesearch/bleve/analysis/lang/pt"
+
+	_ "github.com/blevesearch/bleve/index/store/goleveldb"
+)
+
+type ResponseType int8
+
+const (
+	RedirectResponse ResponseType = iota
+	DataResponse
+	NoResponse
+)
+
+// CachedResponse cache the answer to an URL in the zim
+type CachedResponse struct {
+	ResponseType ResponseType
+	Data         []byte
+	MimeType     string
+}
+
+var (
+	port       = flag.Int("port", -1, "port to listen to, read HOST env if not specified, default to 8080 otherwise")
+	zimPath    = flag.String("path", "", "path for the zim file")
+	indexPath  = flag.String("index", "", "path for the index file")
+	mmap       = flag.Bool("mmap", false, "use mmap")
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+	Z *zim.ZimReader
+	// Cache is filled with CachedResponse to avoid hitting the zim file for a zim URL
+	cache *lru.ARCCache
+	idx   bool
+	index bleve.Index
+
+	templates map[string]*template.Template
+)
+
+func init() {
+	templates = make(map[string]*template.Template)
+
+	tplBox := rice.MustFindBox("templates")
+
+	registerTemplate("index", tplBox)
+	registerTemplate("browse", tplBox)
+	registerTemplate("search", tplBox)
+	registerTemplate("searchNoIdx", tplBox)
+	registerTemplate("searchResult", tplBox)
+	registerTemplate("about", tplBox)
+}
+
+// registerTemplate load template from rice box and add them to a map[string] call templates
+func registerTemplate(name string, tplBox *rice.Box) {
+	tplString, err := tplBox.String(name + ".html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	templates[name] = template.Must(template.New(name).Parse(tplString))
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	flag.Parse()
+	if *zimPath == "" {
+		log.Fatal("provide a zim file path")
+	}
+
+	if *mmap {
+		log.Println("Using mmap")
+	}
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			for _ = range c {
+				pprof.StopCPUProfile()
+				os.Exit(1)
+			}
+		}()
+	}
+
+	// Do we have an index ?
+	if indexPath != nil && *indexPath != "" {
+		if _, err := os.Stat(*indexPath); err != nil {
+			log.Fatal(err)
+		}
+
+		idx = true
+
+		// open the db
+		var err error
+		index, err = bleve.Open(*indexPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// assets
+	box := rice.MustFindBox("static")
+	fileServer := http.StripPrefix("/static/", http.FileServer(box.HTTPBox()))
+	http.Handle("/static/", fileServer)
+
+	// crompress wiki pages
+	http.HandleFunc("/zim/", makeGzipHandler(zimHandler))
+	z, err := zim.NewReader(*zimPath, *mmap)
+	Z = z
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// tpl
+	http.HandleFunc("/search/", makeGzipHandler(searchHandler))
+	http.HandleFunc("/browse/", makeGzipHandler(browseHandler))
+	http.HandleFunc("/about/", makeGzipHandler(aboutHandler))
+	http.HandleFunc("/robots.txt", robotHandler)
+	http.HandleFunc("/", makeGzipHandler(homeHandler))
+
+	// the need for a cache is absolute
+	// a lot of the same urls will be called repeatedly, css, js ...
+	// avoid to look for those one
+	cache, _ = lru.NewARC(40)
+
+	// default listening to port 8080
+	listenPath := ":8080"
+
+	if len(os.Getenv("PORT")) > 0 {
+		listenPath = ":" + os.Getenv("PORT")
+	}
+
+	if port != nil && *port > 0 {
+		listenPath = ":" + strconv.Itoa(*port)
+	}
+
+	// Opening large indexes could takes minutes on raspberry
+	log.Println("Listening on", listenPath)
+
+	err = http.ListenAndServe(listenPath, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ views.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+
+	"github.com/akhenakh/gozim"
+	"github.com/blevesearch/bleve"
+)
+
+const (
+	ArticlesPerPage = 16
+)
+
+func cacheLookup(url string) (*CachedResponse, bool) {
+	if v, ok := cache.Get(url); ok {
+		c := v.(CachedResponse)
+		return &c, ok
+	}
+	return nil, false
+}
+
+// dealing with cached response, responding directly
+func handleCachedResponse(cr *CachedResponse, w http.ResponseWriter, r *http.Request) {
+	if cr.ResponseType == RedirectResponse {
+		log.Printf("302 from %s to %s\n", r.URL.Path, "zim/"+string(cr.Data))
+		http.Redirect(w, r, "/zim/"+string(cr.Data), http.StatusMovedPermanently)
+	} else if cr.ResponseType == NoResponse {
+		log.Printf("404 %s\n", r.URL.Path)
+		http.NotFound(w, r)
+	} else if cr.ResponseType == DataResponse {
+		log.Printf("200 %s\n", r.URL.Path)
+		w.Header().Set("Content-Type", cr.MimeType)
+		// 15 days
+		w.Header().Set("Cache-control", "public, max-age=1350000")
+		w.Write(cr.Data)
+	}
+}
+
+// the handler receiving http request
+func zimHandler(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Path[5:]
+	// lookup in the cache for a cached response
+	if cr, iscached := cacheLookup(url); iscached {
+		handleCachedResponse(cr, w, r)
+		return
+
+	} else {
+		var a *zim.Article
+		a, _ = Z.GetPageNoIndex(url)
+
+		if a == nil {
+			cache.Add(url, CachedResponse{ResponseType: NoResponse})
+		} else if a.EntryType == zim.RedirectEntry {
+			ridx, err := a.RedirectIndex()
+			if err != nil {
+				cache.Add(url, CachedResponse{ResponseType: NoResponse})
+			} else {
+				ra, err := Z.ArticleAtURLIdx(ridx)
+				if err != nil {
+					cache.Add(url, CachedResponse{ResponseType: NoResponse})
+				} else {
+					cache.Add(url, CachedResponse{
+						ResponseType: RedirectResponse,
+						Data:         []byte(ra.FullURL())})
+				}
+			}
+		} else {
+			data, err := a.Data()
+			if err != nil {
+				cache.Add(url, CachedResponse{ResponseType: NoResponse})
+			} else {
+				cache.Add(url, CachedResponse{
+					ResponseType: DataResponse,
+					Data:         data,
+					MimeType:     a.MimeType(),
+				})
+			}
+		}
+
+		// look again in the cache for the same entry
+		if cr, iscached := cacheLookup(url); iscached {
+			handleCachedResponse(cr, w, r)
+		}
+	}
+}
+
+// homeHandler is displaying the / page but redirect every other requests to /zim/xxx
+// some zim files have / hardcoded in their pages
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	var mainURL string
+
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/zim"+r.URL.Path, http.StatusMovedPermanently)
+		return
+	}
+
+	mainPage, err := Z.MainPage()
+	var hasMainPage bool
+
+	if err != nil && mainPage != nil {
+		hasMainPage = true
+		mainURL = "/zim/" + mainPage.FullURL()
+	}
+
+	d := map[string]interface{}{
+		"Path":        path.Base(*zimPath),
+		"Count":       strconv.Itoa(int(Z.ArticleCount)),
+		"IsIndexed":   idx,
+		"HasMainPage": hasMainPage,
+		"MainURL":     mainURL,
+	}
+	templates["index"].Execute(w, d)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+
+	d := map[string]interface{}{}
+	templates["about"].Execute(w, d)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+    pageString := r.FormValue("page")
+    pageNumber, _ := strconv.Atoi(pageString)
+    previousPage := pageNumber - 1
+	if pageNumber == 0 {
+		previousPage = 0
+	}
+    nextPage := pageNumber + 1
+	q := r.FormValue("search_data")
+	d := map[string]interface{}{
+        "Query": q,
+		"Path": path.Base(*zimPath),
+		"Page":         pageNumber,
+		"PreviousPage": previousPage,
+		"NextPage":     nextPage,
+	}
+
+	if !idx {
+		templates["searchNoIdx"].Execute(w, d)
+		return
+	}
+	if q == "" {
+		templates["search"].Execute(w, d)
+		return
+	}
+    itemCount := 20
+    from := itemCount * pageNumber
+    query := bleve.NewQueryStringQuery(q)
+    search := bleve.NewSearchRequestOptions(query, itemCount, from, false)
+	search.Fields = []string{"Title"}
+
+	sr, err := index.Search(search)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	if sr.Total > 0 {
+		d["Info"] = fmt.Sprintf("%d matches for query [%s], took %s", sr.Total, q, sr.Took)
+
+		// Constructs a list of Hits
+		var l []map[string]string
+
+		for _, h := range sr.Hits {
+			idx, err := strconv.Atoi(h.ID)
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			a, err := Z.ArticleAtURLIdx(uint32(idx))
+			if err != nil {
+				continue
+			}
+			l = append(l, map[string]string{
+				"Score": strconv.FormatFloat(h.Score, 'f', 1, 64),
+				"Title": a.Title,
+				"URL":   "/zim/" + a.FullURL()})
+
+		}
+		d["Hits"] = l
+
+	} else {
+		d["Info"] = fmt.Sprintf("No match for [%s], took %s", q, sr.Took)
+		d["Hits"] = 0
+	}
+
+	templates["searchResult"].Execute(w, d)
+}
+
+// browseHandler is browsing the zim page per page
+func browseHandler(w http.ResponseWriter, r *http.Request) {
+	var page, previousPage, nextPage int
+
+	p := r.URL.Query().Get("page")
+	if p != "" {
+		page, _ = strconv.Atoi(p)
+	}
+
+	if page*ArticlesPerPage-1 >= int(Z.ArticleCount) {
+		http.NotFound(w, r)
+		return
+	}
+
+	Articles := make([]*zim.Article, ArticlesPerPage)
+	var pos int
+	for i := page * ArticlesPerPage; i < page*ArticlesPerPage+ArticlesPerPage; i++ {
+		a, err := Z.ArticleAtURLIdx(uint32(i))
+		if err != nil {
+			continue
+		}
+		if a.Title == "" {
+			a.Title = a.FullURL()
+		}
+		Articles[pos] = a
+		pos++
+	}
+
+	if page == 0 {
+		previousPage = 0
+	} else {
+		previousPage = page - 1
+	}
+
+	if page*ArticlesPerPage-1 >= int(Z.ArticleCount) {
+		nextPage = page
+	} else {
+		nextPage = page + 1
+	}
+
+	d := map[string]interface{}{
+		"Page":         page,
+		"PreviousPage": previousPage,
+		"NextPage":     nextPage,
+		"Articles":     Articles,
+	}
+	templates["browse"].Execute(w, d)
+}
+
+func robotHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "User-agent: *\nDisallow: /\n")
+}
