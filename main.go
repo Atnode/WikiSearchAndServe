3e5de8180b079dffbf7961f11459c776d7f4a0b4
Atnode <git@atnode.fr>
2019-12-10 16:22:03 +0100

    Force the use of an index file and remove cpuprofile

--- main.go
+++ main.go
@@ -6,14 +6,12 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
-	"runtime/pprof"
 	"strconv"
 
-	"github.com/GeertJohan/go.rice"
-	"github.com/akhenakh/gozim"
+	rice "github.com/GeertJohan/go.rice"
+	zim "github.com/akhenakh/gozim"
 	"github.com/blevesearch/bleve"
-	"github.com/hashicorp/golang-lru"
+	lru "github.com/hashicorp/golang-lru"
 
 	_ "github.com/blevesearch/bleve/analysis/lang/ar"
 	_ "github.com/blevesearch/bleve/analysis/lang/cjk"
@@ -44,11 +42,10 @@ type CachedResponse struct {
 }
 
 var (
-	port       = flag.Int("port", -1, "port to listen to, read HOST env if not specified, default to 8080 otherwise")
-	zimPath    = flag.String("path", "", "path for the zim file")
-	indexPath  = flag.String("index", "", "path for the index file")
-	mmap       = flag.Bool("mmap", false, "use mmap")
-	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	port      = flag.Int("port", -1, "port to listen to, read HOST env if not specified, default to 8080 otherwise")
+	zimPath   = flag.String("path", "", "path for the zim file")
+	indexPath = flag.String("index", "", "path for the index file")
+	mmap      = flag.Bool("mmap", false, "use mmap")
 
 	Z *zim.ZimReader
 	// Cache is filled with CachedResponse to avoid hitting the zim file for a zim URL
@@ -93,26 +90,13 @@ func main() {
 		log.Println("Using mmap")
 	}
 
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-		go func() {
-			for _ = range c {
-				pprof.StopCPUProfile()
-				os.Exit(1)
-			}
-		}()
-	}
-
 	// Do we have an index ?
-	if indexPath != nil && *indexPath != "" {
+	if indexPath != nil && *indexPath != "" || *indexPath == "" {
+
+		if *indexPath == "" {
+			log.Fatal("provide an index path")
+		}
+
 		if _, err := os.Stat(*indexPath); err != nil {
 			log.Fatal(err)
 		}
